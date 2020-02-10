e13cda985afd479873fd947057063fa4a4de98fb
JuJu <58866597+jujubrains@users.noreply.github.com>
2020-02-09 20:31:13 -0500

    redo server.js

--- server.js
+++ server.js
@@ -1,63 +1,53 @@
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-
-var app = express();
-var port = process.env.PORT || 3000;
-
-app.use(express.static("public"));
+const express = require("express");
+const path = require("path");
+const fs = require("fs")
+const db = require("./db/db.json")
+const app = express();
+app.use(express.static("public"))
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 7000;
 
-app.get("/api/notes", (req,res) => {
-  fs.readFile("./db/db.json", "utf-8", (err, data)=> {
-    const json = JSON.parse(data);
-    res.json(json);
-  });
-});
+console.log(db)
 
-app.post("/api/notes",(req, res) => {
-  const newNote = req.body;
-  // console.log(req.body)
-  // const parsedNote = JSON.stringify(newNote);
-  fs.readFile("./db/db.json", "utf-8", (err,data) => {
-    // res.send(data);
-    const parsedArray = JSON.parse(data);
-    parsedArray.push(newNote);
-    console.log(parsedArray);
-    const stringifyArray = JSON.stringify(parsedArray);
-    fs.writeFile("./db/db.json", stringifyArray,"utf-8", (err) => {
-      if(err) throw err;
-  
-    });
-    res.json(parsedArray);
-  });
-});
+app.get("/api/notes", function(req, res) {
+    res.json(db);
+})
 
-app.delete("/api/notes/:id", (req, res) => {
-  const query = req.params;
-  fs.readFile("./db/db.json", "utf-8", (err,data) => {
-    const parsedArray = JSON.parse(data);
-    idArray = parsedArray.map((note,index) => {
-      note.id = index;
-    });
-    const filteredArr = parsedArray.filter(note => note.id !==
-      parseInt(query.id));
+app.post("/api/notes", function(req, res){
+    let id = db[db.length-1].id+1;
+    let data = req.body;
+    data["id"]=id;
+    console.log(data);
+    db.push(data);
+    console.log(db);
+    res.redirect("/notes");
+})
 
-    fs.writeFile("./db/db.json", filteredArr, "utf-8", err =>  {
-      if (err) throw err;
-    });
-    res.json(filteredArr);
-  });
-});
+// app.delete("/api/notes/:id", function(req, res) {
+//     let data = req.body;
+//     console.log(req.params.id);
+//     db.filter(!req.params.id);
+//     console.log(req.params.id);
+//     res.redirect("/notes");
+// //   function filterid() {
+//        let id = req.params.id;
+//        for (let i = 0; i < db.length; i++) {
+//          const element = array[i];
+         
+//        }
+// //   return age >= 18;
+// // }
+// })
 
-app.get("/notes",(req,res) => {
+app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("*",(req, res) => {
-  return res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-
-app.listen(port, console.log(`server is running on port ${port}`));
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
