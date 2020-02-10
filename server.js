5eed2d322cce12d05c349c39e06a76ce48e0f7fd
JuJu <58866597+jujubrains@users.noreply.github.com>
2020-02-10 16:21:52 -0500

    finish delete and write to file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs")
-const db = require("./db/db.json")
+let db = require("./db/db.json")
 const app = express();
 app.use(express.static("public"))
 app.use(express.json());
@@ -17,28 +17,32 @@ app.get("/api/notes", function(req, res) {
 app.post("/api/notes", function(req, res){
     let id = db[db.length-1].id+1;
     let data = req.body;
-    data["id"]=id;
+    data.id = id;
     console.log(data);
     db.push(data);
     console.log(db);
     res.redirect("/notes");
+    fs.writeFile("./db/db.json", JSON.stringify(db), "utf-8", function (err){
+      if (err) {
+        return console.log(err);
+      }
+    })
 })
 
-// app.delete("/api/notes/:id", function(req, res) {
-//     let data = req.body;
-//     console.log(req.params.id);
-//     db.filter(!req.params.id);
-//     console.log(req.params.id);
-//     res.redirect("/notes");
-// //   function filterid() {
-//        let id = req.params.id;
-//        for (let i = 0; i < db.length; i++) {
-//          const element = array[i];
-         
-//        }
-// //   return age >= 18;
-// // }
-// })
+app.delete("/api/notes/:id", function(req, res) {
+    let notes = req.body;
+    console.log(req.params.id);
+    const newArray = db.filter(note => note.id!=req.params.id)
+    console.log(newArray)
+    console.log(req.params.id);
+    db=newArray;
+    res.json(db);
+    fs.writeFile("./db/db.json", JSON.stringify(db), "utf-8", function (err){
+      if (err) {
+        return console.log(err);
+      }
+    })
+})
 
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
